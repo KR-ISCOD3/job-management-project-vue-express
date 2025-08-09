d568a041d517e947a4c4780f7b51d0dc975de386
KR-ISCOD3 <krisjenxz@gmail.com>
2025-08-10 06:33:57 +0700

    logo-update

--- src/views/Dashboard/components/SideBar.vue
+++ src/views/Dashboard/components/SideBar.vue
@@ -9,15 +9,19 @@
         <div class="w-full h-full ">
             <!-- Logo Website -->
             <div class="w-full text-center border-b-1 border-gray-400 py-2">
-                <img src="" alt="" class="w-[100px] h-[100px] bg-gray-50 mx-auto">
+                <img src="../../../assets/image/logo.png" alt="" class="w-[100px] mx-auto">
                 <h1 class="text-xl text-white font-medium mt-2">ETEC-JOB</h1>
                 <p class="text-sm text-gray-400">Work with Us not <br> with Thai</p>
             </div>
             <!-- Logo Website -->
 
             <!-- Menu -->
-            <ul>
-
+            <ul class="mt-3">
+                <router-link>
+                    <li class="hover:bg-blue-950 transition p-2 text-white rounded-lg">
+                        Dashboard
+                    </li>
+                </router-link>
             </ul>
             <!-- Menu -->
 
