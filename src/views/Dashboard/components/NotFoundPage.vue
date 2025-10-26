bb7336e8ae990cc50da92754e8d6a8272a66098d
KR-ISCOD3 <krisjenxz@gmail.com>
2025-10-26 12:56:44 +0700

    new-move

--- src/views/Dashboard/components/NotFoundPage.vue
+++ src/views/Dashboard/components/NotFoundPage.vue
@@ -3,7 +3,7 @@
 <template>
       <div class="w-[100%] h-[100vh] flex items-center justify-center lg:hidden">
         <div class="text-center">
-            <img src="../../../../../src/assets/image/giphy.gif" alt="" class="w-[300px] h-[300px] object-cover">
+            <img src="/image/giphy.gif" alt="" class="w-[300px] h-[300px] object-cover">
             <p class="font-bold mt-2">PAGE NOT FOUND</p>
         </div>
     </div>
